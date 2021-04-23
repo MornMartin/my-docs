8f7e7ee02b5286353dd89dabadc83582326c69d3
Martin <beingwrong@live.com>
2021-04-24 00:32:26 +0800

    create 【C++程序设计1.1.1】

---
+++ 自考/04737C++程序设计/README.md
@@ -0,0 +1,3 @@
+1. 安装mingw64并配置环境变量
+2. 使用vscode打开该目录
+3. 替换c_cpp_properties.json与launch.json文件中mingw64的安装目录
---
+++ 自考/04737C++程序设计/cin.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"请输入加数：";
+    cin>>a;
+    cout<<"请输入被加数：";
+    cin>>b;
+    cout<<"你输入的加数是："<<a<<", 你输入的被加数是："<<b;
+    cout<<"两数之和是："<<a+b
+    <<endl;
+}
