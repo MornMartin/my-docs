4ae8ea2c8b0cc8171ec7945a3dc12292958cf50a
Martin <beingwrong@live.com>
2021-05-11 00:03:46 +0800

    create 【C++程序设计2.1-2.3】

---
+++ 自考/04737C++程序设计/2.3-class.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Date {
+    public: 
+        int year;
+        int month;
+        int date;
+};
+
+class Persion {
+    private:
+        string name;
+        Date birthDay;
+    public:
+        Persion();// 与class同名的成员函数为构造函数、不能指定返回值类型
+        Persion(string name);
+        string say();
+        int getAge(int currentYear);
+};
+// 在类体外定义成员函数
+
+Persion::Persion() {
+ // do nothing
+};
+Persion::Persion(string name) {
+    // 访问成员变量直接用变量名
+    name = name;
+};
+int Persion::getAge(int currentYear) {
+    // 访问其他对象的成员变量使用"."
+    return currentYear - birthDay.year;
+}
+string Persion::say() {
+    return "Hello, I'm " + name + ". I'm ";
+};
